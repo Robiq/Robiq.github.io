7877ceec46b2015ab028d081fb8304e5435ab176
Robiq <robin@wub.no>
2017-10-17 11:27:47 +0900

    Working finished

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// Create our number formatter.
+var formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'JPY',
+  minimumFractionDigits: 2,
+});
+
+var sum = 0;
+
+
 function calculate(id){
 	val = document.getElementById("p_"+id);
 	val = val.value;
@@ -6,9 +16,9 @@ function calculate(id){
 	amtInt = parseInt(amt.value);
 	amtInt++;
 	amt.value = amtInt;
-	sum = parseInt(amtInt * val, 10);
-	console.log("val: " + val + " amt: " + amt.value + " sum: " + sum);
-	document.getElementById("i"+id).innerHTML = "Total: " + sum;
+	sumL = parseInt(amtInt * val, 10);
+	console.log("val: " + val + " amt: " + amt.value + " sum: " + sumL);
+	document.getElementById("i"+id).innerHTML = "Quantity: " + amt.value + " Total: " + formatter.format(sumL);
 	return false;
 }
 
@@ -18,9 +28,9 @@ function summarize(id){
 	val = parseInt(val, 10);
 	amt = document.getElementById(id+"amt").value;
 	amtInt = parseInt(amt);
-	sum = parseInt(amtInt * val, 10);
-	console.log("Summ: val: " + val + " amt: " + amt + " sum: " + sum);
-	return sum;
+	sumL = parseInt(amtInt * val, 10);
+	console.log("Summ: val: " + val + " amt: " + amt + " sum: " + sumL);
+	return sumL;
 }
 
 function totalSum(){
@@ -28,6 +38,25 @@ function totalSum(){
 	for (var i = 1; i < 4; i++) {
 		sum += summarize(i);
 	}
-	document.getElementById("total sum").innerHTML = "Total sum is: " + sum + ".";	
+	document.getElementById("total sum").innerHTML = "Total sum is: " + formatter.format(sum) + ".";	
 	return false;
+}
+
+function showPrices(btn) {
+	prices = document.getElementById("prices");
+	obj2 = document.getElementById("pTxt");
+	if (prices.style.display === "none") {
+        prices.style.display = "block";
+        obj2.style.display = "block";
+        btn.innerHTML = "Hide prices!"; 
+    } else {
+        prices.style.display = "none";
+        obj2.style.display = "none";
+        btn.innerHTML = "Show prices!";
+    }
+	return false;
+}
+
+function buy(){
+	document.body.innerHTML = "<h2>Your purchase is: " + formatter.format(sum) +"</h2>";
 }
