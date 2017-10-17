f290396be607eb1c39eb6f9b8ba670857ee38081
Robiq <robin@wub.no>
2017-10-17 10:59:19 +0900

    Working

--- index.js
+++ index.js
@@ -2,24 +2,32 @@ function calculate(id){
 	val = document.getElementById("p_"+id);
 	val = val.value;
 	val = parseInt(val, 10);
-	//console.log(val);
-	amt = document.getElementById(id+"amt").value;
-	amtInt = parseInt(amt);
-	console.log(amtInt);
+	amt = document.getElementById(id+"amt");
+	amtInt = parseInt(amt.value);
 	amtInt++;
 	amt.value = amtInt;
 	sum = parseInt(amtInt * val, 10);
-	console.log(sum);
+	console.log("val: " + val + " amt: " + amt.value + " sum: " + sum);
 	document.getElementById("i"+id).innerHTML = "Total: " + sum;
+	return false;
+}
 
+function summarize(id){
+	val = document.getElementById("p_"+id);
+	val = val.value;
+	val = parseInt(val, 10);
+	amt = document.getElementById(id+"amt").value;
+	amtInt = parseInt(amt);
+	sum = parseInt(amtInt * val, 10);
+	console.log("Summ: val: " + val + " amt: " + amt + " sum: " + sum);
 	return sum;
 }
 
 function totalSum(){
 	sum = 0;
 	for (var i = 1; i < 4; i++) {
-		sum += calculate(i);
-		console.log(i + " sum: " + sum);
+		sum += summarize(i);
 	}
 	document.getElementById("total sum").innerHTML = "Total sum is: " + sum + ".";	
+	return false;
 }
